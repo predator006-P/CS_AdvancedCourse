c3ff58b61db4762dc59d09942d06bb75b08ce6e4
pnemeth <pnemeth@LT000305>
2020-05-05 17:55:43 +0200

    With custom delegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -7,8 +7,20 @@ namespace Delegates
         static void Main(string[] args)
         {
             var photoProcessor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            var photo = new Photo();
 
-            photoProcessor.Process("C:\\");
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+
+            filterHandler += filters.ApplyContrast;
+            filterHandler += Test;
+
+            photoProcessor.Process("photo.jpg", filterHandler);
+        }
+
+        static void Test(Photo photo)
+        {
+            Console.WriteLine("\nTEST!!!\n");
         }
     }
 }
