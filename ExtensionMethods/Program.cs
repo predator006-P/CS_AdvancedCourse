d5fbbc5098799f20a6b9bf2b9df81fe0286cd00b
Peti <predator006@freemail.hu>
2020-05-27 23:33:04 +0200

    Extension methods finished.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    //Adding methods to an exsiting class without:
+    // - Changing its source code
+    // - Creating new class that inherits from it
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposd to be a very long post blah blah blah ";
+            Console.WriteLine(post);
+            var shortenedPost = post.Shorten(5);
+            Console.WriteLine(shortenedPost + "...");
+        }
+    }
+}
