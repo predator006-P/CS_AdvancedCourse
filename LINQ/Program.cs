7a0354cb9e6d458cc62493a270cde24c27b95284
Peti <predator006@freemail.hu>
2020-05-28 12:01:39 +0200

    LINQ Query Operators vs. LINQ Extension Methods

---
+++ LINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            // LINQ Query Operators
+            var cheaperBooks =
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b.Title;
+
+            // LINQ Extension Methods
+            var cheapBooks = books
+                                .Where(b => b.Price < 10)  //filter
+                                .OrderBy(b => b.Title)     //sort
+                                .Select(b => b.Title);     //select
+
+            foreach (var book in cheaperBooks)
+            {
+               // Console.WriteLine(book.Title + " " + book.Price);
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
