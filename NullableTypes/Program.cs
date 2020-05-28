912d7e18caa2bc73e541d26a9a1c68cad72b9d28
Peti <predator006@freemail.hu>
2020-05-28 15:49:04 +0200

    Nullable types first commit.

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            bool b = true;
+
+            DateTime? date1 = null;
+            Nullable<DateTime> date2 = null;
+
+            Console.WriteLine(date1.GetValueOrDefault());
+            Console.WriteLine(date1.HasValue);
+            Console.WriteLine(date1.Value);//it will throw an InvalidOperationException
+        }
+    }
+}
