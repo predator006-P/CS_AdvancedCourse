9f9814a3353ea9cb2062786877490e5a4342c2d9
Peti <predator006@freemail.hu>
2020-05-31 14:46:10 +0200

    Dynamic

---
+++ DynamicBinding/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "Peti";
+            name = 10;
+        }
+    }
+}
