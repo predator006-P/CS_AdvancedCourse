b05b3108117b0fd600ce11158fbe5f63303d420d
Peti <predator006@freemail.hu>
2020-05-31 17:50:49 +0200

    Custom exception for covering deeper level exceptions.

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,25 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ExceptionHandling
 {
+    public class Video
+    {
+        public string Title { get; set; }
+    }
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access YouTube web service
+                // Read data
+                // Create a list of Video objects
+
+                throw new Exception("Opps, some YouTube error happened....");
+            }
+            catch (Exception ex)
+            {
+                // Log 
+                throw new YouTubeException("Could not fetch the videos from YouTube", ex);
+            }
+            return new List<Video>();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                using (var streamReader = new StreamReader(@"c:\file.zip")) 
-                {
-                    var content = streamReader.ReadToEnd();
-
-                }
-                throw new Exception("Oopsz");
+                var youTubeApi = new YouTubeApi();
+                youTubeApi.GetVideos("Peti");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
         }
     }
 }
