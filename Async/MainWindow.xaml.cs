c982bc1b791cc0c62e05aa20277fcef9eb8d9e91
Peti <predator006@freemail.hu>
2020-06-04 14:35:22 +0200

    Async first section.

---
+++ Async/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Async
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHtmlAsync("http://msdn.microsoft.com");
+        }
+
+        public async Task DownloadHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);
+
+            using (var streamWriter = new StreamWriter(@"C:\Users\Peti\Source\Repos\predator006-P\CS_AdvancedCourse\Async\result.html"))
+            {
+                await streamWriter.WriteAsync(html);
+            }
+        }
+        public void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+
+            using (var streamWriter = new StreamWriter(@"C:\Users\Peti\Source\Repos\predator006-P\CS_AdvancedCourse\Async\result.html"))
+            {
+                streamWriter.Write(html);
+            }
+        }
+    }
+}
